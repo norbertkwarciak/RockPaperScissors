427a5641f2bd8a4c579cb0b290b053cdc7016eae
norbertkwarciak <norbert.kwarciak@gmail.com>
2017-11-25 19:30:04 +0100

    uglify installed

--- gruntfile.js
+++ gruntfile.js
@@ -1,51 +1,64 @@
 module.exports = function(grunt) {
     
-      // Project configuration.
-      grunt.initConfig({
-          sass: {
-              options: {
-                sourceMap: true
-              },
-              dist: {
-            files: [{
-               expand: true,
-               cwd: 'sass',
-               src: ['**/*.scss'],
-               dest: 'css',
-               ext: '.css'
-            }]
-              }
-        },
-    
-        imagemin: {
-          dynamic: {
-              files: [{
-                  expand: true,
-                  cwd: 'images/',
-                  src: ['**/*.{png,jpg,gif}'],
-                  dest: 'images/build/'
-              }]
+  // Project configuration.
+  grunt.initConfig({
+      sass: {
+          options: {
+            sourceMap: true
+          },
+          dist: {
+        files: [{
+            expand: true,
+            cwd: 'sass',
+            src: ['**/*.scss'],
+            dest: 'css',
+            ext: '.css'
+        }]
           }
-        },
-    
-        watch: { // Compile everything into one task with Watch Plugin
-          css: {
-            files: '**/*.scss',
-            tasks: ['sass'],
-            options: {
-                spawn: false,
-            }
-          }
+    },
+
+    imagemin: {
+      dynamic: {
+          files: [{
+              expand: true,
+              cwd: 'images/',
+              src: ['**/*.{png,jpg,gif}'],
+              dest: 'images/build/'
+          }]
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'dest/js/app.min.js': ['js/scripts.js']
         }
-    
-      });
-      // Load the plugins tasks
-      grunt.loadNpmTasks('grunt-sass');
-      grunt.loadNpmTasks('grunt-contrib-imagemin');
-      grunt.loadNpmTasks('grunt-contrib-watch');
-    
-    
-      // Default task(s).
-    
-      grunt.registerTask('default', ['sass', 'imagemin'], ['watch']);
-    };
+      }
+    },
+
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+        }
+      },
+
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['uglify'],
+        options: {
+          atBegin: true,
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass', 'imagemin', 'uglify'], ['watch']);
+};
