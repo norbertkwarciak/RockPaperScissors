4228381f3ee9010e3374a6ac652011542c59e3ed
norbertkwarciak <norbert.kwarciak@gmail.com>
2017-12-10 21:12:58 +0100

    minor code fixes

--- gruntfile.js
+++ gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
     uglify: {
       my_target: {
         files: {
-          'dest/js/app.min.js': ['js/scripts.js']
+          'js/app.min.js': ['js/scripts.js']
         }
       }
     },
